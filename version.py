b2110b2b43f0fb4f90ab8943c379789d648fc0b5
HBrydon <2030784+HBrydon@users.noreply.github.com>
2023-09-22 14:42:02 -0500

    20230922 espdude Initial

---
+++ version.py
@@ -0,0 +1,15 @@
+# version.py
+#
+# Generated file - do not edit
+#
+
+BUILD_DATE         ="20230922"
+YYYYMMDD           ="2023-09-22"
+VER_MAJOR          =0
+VER_MINOR          =1
+VER_REVISION       ="20230922b1"
+VERSION            ="0.1.20230922b1"
+plugin_version     ="0.1.20230922b1"
+__plugin_version__ ="0.1.20230922b1"
+FULLNAME           ="H Brydon"
+EMAIL              ="2030784+HBrydon@users.noreply.github.com"
