e0e362870d0255c80e8d98db5d2940b336d82b8e
HBrydon <2030784+HBrydon@users.noreply.github.com>
2023-10-05 16:55:48 -0500

    Update to newer syntax

--- octoprint_filamentswitcher/static/js/filamentswitcher.js
+++ octoprint_filamentswitcher/static/js/filamentswitcher.js
@@ -34,17 +34,17 @@ $(function() {
 
     // This is how our plugin registers itself with the application, by adding some configuration
     // information to the global variable OCTOPRINT_VIEWMODELS
-    OCTOPRINT_VIEWMODELS.push([
+    OCTOPRINT_VIEWMODELS.push({
         // This is the constructor to call for instantiating the plugin
-        FilamentSwitcherViewModel,
+        construct: FilamentSwitcherViewModel,
 
         // This is a list of dependencies to inject into the plugin, the order which you request
         // here is the order in which the dependencies will be injected into your view model upon
         // instantiation via the parameters argument
-        ["settingsViewModel"],
+        dependencies: ["settingsViewModel"],
 
         // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#tab_plugin_filamentswitcher"]
-    ]);
+        elements: ["#tab_plugin_filamentswitcher"]
+    });
 });
 
