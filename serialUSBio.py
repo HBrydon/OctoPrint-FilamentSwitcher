3063862138d6d210a88b1acd40c3a776de0f46ad
HBrydon <2030784+HBrydon@users.noreply.github.com>
2023-10-05 16:58:13 -0500

    Add port loggint

--- serialUSBio.py
+++ serialUSBio.py
@@ -9,14 +9,15 @@ import serialLogger
 class SerialUSBio:
     def __init__(self, port='/dev/ttyUSB0', logfile='inout.log'):
         #print('*** SerialUSBio(<', port, '>, <', logfile, '>)')
+        #self.port = port
+        #self.logfile = logfile
         self.ser = serial.Serial(port, 115200)
         self.data_queue = queue.Queue()
         self.consumer_thread = threading.Thread(target=self.buffered_read_thread)
         time.sleep(1)  # Allow serial device to settle
-        self._serialLogger = serialLogger.serialLogger(logfile)
+        self._serialLogger = serialLogger.serialLogger(logfile, port)
 
     def __del__(self):
-        self._serialLogger.log_message(logging.INFO, "Closing Serial USB port")
         self.stop()
         self.ser = None
         self._serialLogger = None
@@ -28,6 +29,12 @@ class SerialUSBio:
     def write_line(self, data):
         self.ser.write(data.encode())
         self.ser.write(b"\n")
+        self._serialLogger.log_send_message(data)
+        #print(f"Sending: {data}")
+
+    # (producer) Write record to the device
+    def write(self, data):
+        self.ser.write(data.encode())
         self._serialLogger.log_send_message(data)
         #print(f"Sending: {data}")
 
@@ -44,10 +51,14 @@ class SerialUSBio:
         return self.data_queue.qsize()
 
     def read_line_from_queue(self):
-        return self.data_queue.get()
+        try:
+            return self.data_queue.get_nowait()
+        except queue.Empty:
+            return ""
 
     def stop(self):
         if self.ser.isOpen():
+            self._serialLogger.log_message("Closing Serial USB port")
             self.ser.close()
 
 
