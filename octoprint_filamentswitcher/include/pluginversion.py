274234178a89e7e421e70276c4df8c351c743299
HBrydon <2030784+HBrydon@users.noreply.github.com>
2023-10-06 22:26:07 -0500

    Move source files to new location

---
+++ octoprint_filamentswitcher/include/pluginversion.py
@@ -0,0 +1,17 @@
+# version.py
+#
+# Generated file - do not edit
+#
+
+BUILD_DATE         ="2023-10-06"
+BUILD_YMD          ="20231006"
+VER_MAJOR          =0
+VER_MINOR          =1
+VER_PATCH          ="20231006"
+VER_REVISION       ="20231006b12"
+VER_MMP            ="0.1.20231006"
+VERSION            ="0.1.20231006b12"
+plugin_version     ="0.1.20231006b12"
+__plugin_version__ ="0.1.20231006b12"
+FULLNAME           ="H Brydon"
+EMAIL              ="2030784+HBrydon@users.noreply.github.com"
