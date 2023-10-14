25c93b0169c9b46900033d8f5d203a9af36c9478
HBrydon <2030784+HBrydon@users.noreply.github.com>
2023-10-14 15:01:38 -0500

    20231014b1 espdude

---
+++ octoprint_filamentswitcher/include/pluginversion.py
@@ -0,0 +1,15 @@
+# version.py
+#
+# Generated file - do not edit
+#
+
+BUILD_DATE         ="2023-10-14"
+BUILD_YMD          ="20231014"
+VER_MAJOR          =0
+VER_MINOR          =1
+VER_PATCH          ="20231014"
+VER_REVISION       ="20231014b1"
+VER_MMP            ="0.1.20231014"
+VERSION            ="0.1.20231014b1"
+FULLNAME           ="H Brydon"
+EMAIL              ="2030784+HBrydon@users.noreply.github.com"
