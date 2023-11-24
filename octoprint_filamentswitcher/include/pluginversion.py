eb39981c002d4971931c6a428570b929cac0ac50
HBrydon <2030784+HBrydon@users.noreply.github.com>
2023-11-23 20:15:45 -0600

    Daily build

--- octoprint_filamentswitcher/include/pluginversion.py
+++ octoprint_filamentswitcher/include/pluginversion.py
@@ -3,13 +3,13 @@
 # Generated file - do not edit
 #
 
-BUILD_DATE         ="2023-10-16"
-BUILD_YMD          ="20231016"
+BUILD_DATE         ="2023-11-23"
+BUILD_YMD          ="20231123"
 VER_MAJOR          =0
 VER_MINOR          =1
-VER_PATCH          ="20231016"
-VER_REVISION       ="20231016b2"
-VER_MMP            ="0.1.20231016"
-VERSION            ="0.1.20231016b2"
+VER_PATCH          ="20231123"
+VER_REVISION       ="20231123b2"
+VER_MMP            ="0.1.20231123"
+VERSION            ="0.1.20231123b2"
 FULLNAME           ="H Brydon"
 EMAIL              ="2030784+HBrydon@users.noreply.github.com"
